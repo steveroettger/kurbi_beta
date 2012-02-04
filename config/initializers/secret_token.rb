4098cc6c21c4fc08e6c9bafeb027ce5217ec7bf9
Steve Roettger <steve@steveroettger.com>
2012-02-04 11:17:39 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KurbiBeta::Application.config.secret_token = 'bfc5e89fea4d55a560a674cbac236fa2c1ac1dd3188d48bedb2d060176232b40d45f0a49ad23b3240fd1b8514bae0999ba6f69f2b6f9164ff04a85447ee47d79'
