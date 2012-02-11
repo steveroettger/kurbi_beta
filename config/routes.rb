94006d18c1f3200d678d3c9df5c8e1a187edc755
matteckman <matteckman@gmail.com>
2012-02-11 15:05:51 -0500

    added default route

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ KurbiBeta::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-  
-  match 'home/' => 'home#index'
+
+  root :to => 'home#index'  
   
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
