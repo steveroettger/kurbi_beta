855b0618785e6945f4f25e1ea541d9a5aab93a30
Steve Roettger <steve@steveroettger.com>
2012-02-21 16:44:44 -0500

    Corrected routes issue/created sign up

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,8 +5,12 @@ class Member < ActiveRecord::Base
 	
 	validates_confirmation_of :password
 	validates_presence_of :password, :on => :create
-	validates_presence_of :email
-	validates_uniqueness_of :email
+	validates :password, :length =>  { :within => 6..40 }
+	
+	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email,    :presence => true,
+	                     :format   => { :with => email_regex },
+	                     :uniqueness => { :case_sensitive => false }
 	
 	def encrypt_password
 		if password.present?
